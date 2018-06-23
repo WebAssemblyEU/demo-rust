c6446a7aa07f5e526446c7fa4a6ff187b9bc247d
Thomas Tränkler <github.com@thomas.traenkler.com>
2018-06-24 01:34:35 +0200

    rust webassembly hello world project bootstrapped with wasm-pack

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![feature(proc_macro, wasm_import_module, wasm_custom_section)]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
